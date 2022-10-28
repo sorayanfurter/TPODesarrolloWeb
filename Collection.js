b9dc1b4c92f8f0f94dfb0525a22d4aa1f537d150
Soraya <sorinfurter@gmail.com>
2022-10-28 09:13:12 -0300

    Carrousel seccion Collection

--- Collection.js
+++ Collection.js
@@ -1,40 +1,29 @@
-/*var collection = 0;
-var i = 0;
-var slider=document.getElementsByClassName("slider");
-var line=document.getElementsByClassName("line");
 
-auto();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function show(n){
-    for(i=0;i<slider.length;i++){
-        slider[i].style.display="none";
-    }
-    for(i=0;i<line.length;i++) {
-        line[i].className=line[i].className.replace(" active");
-    }
-    slider[n-1].style.display=("block");
-    line[n-1].className += " active";
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function auto(){
-    collection++;
-    if(collection>slider.length){
-        collection = 1;
-    }
-
-    show(collection);
-    setTimeout(auto, 2000);
-
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function plusSlide(n){
-    collection += n;
-    if (collection> slider.length){
-        collection =1;
-    }
-    if (collection < 1) {
-        collection = slider.length;
-    }
-    show(collection);
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {    slides[i].style.display = "none";
 }
-*/
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
+
